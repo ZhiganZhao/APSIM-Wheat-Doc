f0a1bb846ec62590331cb6d7c26a37c3459c6a70
Bangyou Zheng <zheng.bangyou@gmail.com>
2017-10-31 13:56:23 +1000

    Update the doc

--- _generate-gh-pages.R
+++ _generate-gh-pages.R
@@ -9,10 +9,10 @@ tryCatch({
     }
 
     bookdown::render_book('.', output_format = 'bookdown::gitbook')
-    # bookdown::render_book('.', output_format = 'bookdown::word_document2')
-    # bookdown::render_book('.', output_format = 'bookdown::pdf_book')
-    # bookdown::render_book('.', output_format = 'bookdown::epub_book')
-    # bookdown::calibre('_book/APSIM-Wheat.epub', '_book/APSIM-Wheat.mobi')
+    bookdown::render_book('.', output_format = 'bookdown::word_document2')
+    bookdown::render_book('.', output_format = 'bookdown::pdf_book')
+    bookdown::render_book('.', output_format = 'bookdown::epub_book')
+    bookdown::calibre('_book/APSIM-Wheat.epub', '_book/APSIM-Wheat.mobi')
 
 
     links <- list.files('_book', '*.html', full.names = TRUE)
